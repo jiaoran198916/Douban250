babf526fcb678c2edead86d13521fa1471bcfccc
jiaoran <920495391@qq.com>
2021-03-15 14:38:18 +0800

    mongodb

--- Douban250/pipelines.py
+++ Douban250/pipelines.py
@@ -33,3 +33,11 @@ class Douban250Pipeline:
 #         data = dict(item)
 #         self.post.insert(data)
 #         return item
+# MONGODB 主机名
+# MONGODB_HOST = "127.0.0.1"
+# MONGODB 端口号
+# MONGODB_PORT = 27017
+# 数据库名称
+# MONGODB_DBNAME = "Douban"
+# 存放数据的表名称
+# MONGODB_SHEETNAME = "doubanmovies"
